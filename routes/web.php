4b31b3afb231a90443caf2dd48dd00119e7891e9
jiannsyah <brian8jian@gmail.com>
2024-09-24 15:43:47 +0700

    membuat Resource Project, dengan perintah: php artisan make:resource ProjectResource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
