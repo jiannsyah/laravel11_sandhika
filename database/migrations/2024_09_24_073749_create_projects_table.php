0fbf33a0660d427ce1176b83770f2dd81228635a
jiannsyah <brian8jian@gmail.com>
2024-09-24 14:42:43 +0700

    mengisi data di database & factory

--- database/migrations/2024_09_24_073749_create_projects_table.php
+++ database/migrations/2024_09_24_073749_create_projects_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->timestamp('due_date')->nullable();
+            $table->string('status');
+            $table->timestamp('image_path')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
             $table->timestamps();
         });
     }
